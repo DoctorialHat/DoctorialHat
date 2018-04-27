759e19dfe8fe1c5ae44b42a71bb5ac23bde40040
JacksonYANG <353904675@qq.com>
2018-04-28 01:10:37 +0800

    改进按钮功能,完成我的页面部分功能 新建了activity,对应可完成相应的.xml文件
    Signed-off-by: JacksonYANG <353904675@qq.com>

---
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/FriendsActivity.java
@@ -0,0 +1,15 @@
+package top.jacksonyang.doctorialhat.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import top.jacksonyang.doctorialhat.R;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+    }
+}
