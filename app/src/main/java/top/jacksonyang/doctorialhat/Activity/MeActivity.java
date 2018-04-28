edc4d2d65c44b3a4b356066596f96a7c8c81889c
paco <502115925@qq.com>
2018-04-28 22:33:42 +0800

    1、初步完成“我的”页面，也可实现上下滑动。 2、“通用”下面的按钮，初始颜色为白色，点击之后会变成灰色。 3、设置了强制竖屏。

--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/MeActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/MeActivity.java
@@ -5,8 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import top.jacksonyang.doctorialhat.R;
 
-public class MeActivity extends AppCompatActivity {
 
+public class MeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
