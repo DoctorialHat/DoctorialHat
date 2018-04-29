225a094aa5ae502be720aa83853baa3b18119ac5
paco <502115925@qq.com>
2018-04-29 19:58:46 +0800

    1、“我的”页面增加了分割线 2、完成“登录注册”页面

---
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package top.jacksonyang.doctorialhat.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.jacksonyang.doctorialhat.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
