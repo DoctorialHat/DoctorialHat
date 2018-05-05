67429f9c911a56bd72d5f0254c83153a20717ed2
paco <502115925@qq.com>
2018-05-06 00:20:18 +0800

    1、圆角头像 2、实现页面相互跳转

--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/AboutUs.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/AboutUs.java
@@ -2,14 +2,32 @@ package top.jacksonyang.doctorialhat.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import top.jacksonyang.doctorialhat.R;
 
 public class AboutUs extends AppCompatActivity {
 
+    private ImageView back;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        back=(ImageView)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/MeActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/MeActivity.java
@@ -1,15 +1,45 @@
 package top.jacksonyang.doctorialhat.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import top.jacksonyang.doctorialhat.R;
 
 
 public class MeActivity extends AppCompatActivity {
+
+    private Button aboutUs;
+    private Button myInformation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_me);
+
+        aboutUs = (Button) findViewById(R.id.guanyuwomen);
+        myInformation=(Button)findViewById(R.id.myinformation);
+
+        myInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent meToMyInfo=new Intent(MeActivity.this,MyInfomationActivity.class);
+                startActivity(meToMyInfo);
+            }
+        });
+
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent meToAboutUs = new Intent(MeActivity.this,AboutUs.class);
+                startActivity(meToAboutUs);
+            }
+        });
+
     }
+
+
+
 }
