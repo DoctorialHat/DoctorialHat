cdd962343b1819a95881bf2ba433a4f2c30b9122
Wyman798 <1115921064@qq.com>
2018-05-07 22:11:24 +0800

    完成了注册界面，接下来要把用户密码保存到本地
    Signed-off-by: Wyman798 <1115921064@qq.com>

--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/LoginActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/LoginActivity.java
@@ -2,14 +2,37 @@ package top.jacksonyang.doctorialhat.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import top.jacksonyang.doctorialhat.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button login;//登录
+    private EditText phone;//手机号码
+    private EditText password;//密码
+    private String mphone;
+    private String mpassword;
+    private CheckBox saveuser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login=(Button)findViewById(R.id.button);
+        phone=(EditText)findViewById(R.id.editText);
+        password=(EditText)findViewById(R.id.editText2);
+        mphone=phone.getText().toString();
+        mpassword=password.getText().toString();
+
+        //点击登录按钮
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //if()
+            }
+        });
     }
 }
--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/RegisterActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/RegisterActivity.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import top.jacksonyang.doctorialhat.R;
@@ -19,6 +20,10 @@ public class RegisterActivity extends AppCompatActivity {
     private String mNewPasswd;
     private String mPhone;
     private String mChapcha;
+    //快速登录
+    private ImageView wechat;//微信
+    private ImageView oicq;//QQ
+    private ImageView mblog;//新浪微博
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,20 +33,56 @@ public class RegisterActivity extends AppCompatActivity {
         phone = (EditText) findViewById(R.id.editText);
         NewPasswd = (EditText) findViewById(R.id.editText4);
         captcha = (EditText) findViewById(R.id.editText2);
-
         mNewPasswd = NewPasswd.getText().toString();
         mPhone = phone.getText().toString();
         mChapcha = captcha.getText().toString();
+        //快速登录
+        wechat=(ImageView)findViewById(R.id.imageView11);
+        oicq=(ImageView)findViewById(R.id.imageView12);
+        mblog=(ImageView)findViewById(R.id.constraintLayout13);
         //点击注册按钮
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(TextUtils.isEmpty(mPhone)){
+                    Toast.makeText(RegisterActivity.this, "手机号不能为空", Toast.LENGTH_SHORT).show();
+                    register.setClickable(false);
+                }
                 if (isPhoneNo(mPhone)) {
                     Toast.makeText(RegisterActivity.this, "请输入正确的手机号", Toast.LENGTH_SHORT).show();
                     register.setClickable(false);
                 }
+                //密码最低6位，由字母和数字组合
+                if(TextUtils.isEmpty(mNewPasswd)){
+                    Toast.makeText(RegisterActivity.this,"密码不能为空",Toast.LENGTH_SHORT).show();
+                    register.setClickable(false);
+                }
+                if(isPassword(mNewPasswd)){
+                    Toast.makeText(RegisterActivity.this,"请按正确格式输入密码",Toast.LENGTH_SHORT).show();
+                    register.setClickable(false);
+                }
+                if(TextUtils.isEmpty(mChapcha)){
+                    Toast.makeText(RegisterActivity.this, "验证码不能为空", Toast.LENGTH_SHORT).show();
+                    register.setClickable(false);
+                }
+                if(isChapcha(mChapcha)){
+                    Toast.makeText(RegisterActivity.this, "请输入6位验证码", Toast.LENGTH_SHORT).show();
+                    register.setClickable(false);
+                }
+                //注册成功后，保存用户的手机密码到服务端
+                //此版本保存到本地
+                //代码
             }
         });
+        SendCaptcha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击获取验证码按钮，服务端发送短信给手机，用户获取验证码
+                //代码
+            }
+        });
+        //快速登录部分
+        //代码
     }
     public static boolean isPhoneNo(String phone){
         /*
@@ -52,7 +93,14 @@ public class RegisterActivity extends AppCompatActivity {
         18(3G)号段：180、181、182、183、184、185、186、187、188、189
          */
         String telRege="[1][34578]\\d{9}";
-        if(TextUtils.isEmpty(phone)) return false;
-        else return phone.matches(telRege);
+        return phone.matches(telRege);
+    }
+    public static boolean isPassword(String password){
+        String pasRege="^[a-zA-Z0-9]{6,16}$";
+        return password.matches(pasRege);
+    }
+    public static boolean isChapcha(String chapcha){
+        String chaRege="(?<![0-9])([0-9]{" + 6+ "})(?![0-9])";
+        return chapcha.matches(chaRege);
     }
 }
