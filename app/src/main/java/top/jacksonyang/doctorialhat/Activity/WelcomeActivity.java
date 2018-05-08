0b438f9d4b986c8048dcb64d999c46fc61b1362b
SemiLavender <lf1873463348@gmail.com>
2018-05-08 17:35:10 +0800

    将me里面的登入和注册活动以弹窗的形式显出

--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/WelcomeActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/WelcomeActivity.java
@@ -1,13 +1,19 @@
 package top.jacksonyang.doctorialhat.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import top.jacksonyang.doctorialhat.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(top.jacksonyang.doctorialhat.R.layout.activity_welcome);
+        setContentView(R.layout.activity_welcome);
+        Intent intent=new Intent(WelcomeActivity.this,MeActivity.class);
+        startActivity(intent);
     }
 }
