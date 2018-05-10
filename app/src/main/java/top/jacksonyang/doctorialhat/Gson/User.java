755f6c6789978fd1c64ac8bfe8a55a529a474c38
JacksonYANG <353904675@qq.com>
2018-05-10 19:54:56 +0800

    v1.0 可以测试的版本
    Signed-off-by: JacksonYANG <353904675@qq.com>

--- app/src/main/java/top/jacksonyang/doctorialhat/Gson/User.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Gson/User.java
@@ -1,10 +1,20 @@
 package top.jacksonyang.doctorialhat.Gson;
 
+import com.google.gson.annotations.SerializedName;
+
 import org.litepal.crud.DataSupport;
 
 public class User extends DataSupport {
     private String phone;
     private String password;
+    private String nickName;
+
+    @SerializedName("description")
+    private String des;
+
+    private int rank;
+    private int winRating;
+    private int questionNum;
 
     public String getPhone() {
         return phone;
@@ -20,5 +30,45 @@ public class User extends DataSupport {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public int getWinRating() {
+        return winRating;
+    }
+
+    public void setWinRating(int winRating) {
+        this.winRating = winRating;
+    }
+
+    public int getQuestionNum() {
+        return questionNum;
+    }
+
+    public void setQuestionNum(int questionNum) {
+        this.questionNum = questionNum;
     }
 }
