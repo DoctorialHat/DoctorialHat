f4028c3e15a4cbc8869d3081ab299aaa1b1abb36
paco <502115925@qq.com>
2018-05-15 23:37:32 +0800

    1、优化UI 2、完成“我的信息”页面

---
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/ChangePhoneNumberActivity.java
@@ -0,0 +1,16 @@
+package top.jacksonyang.doctorialhat.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import top.jacksonyang.doctorialhat.R;
+
+
+public class ChangePhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_phone_number);
+    }
+}
