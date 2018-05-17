bbe5b98fa4d9083c55092b757db55364f83e72a0
JacksonYANG <353904675@qq.com>
2018-05-17 11:43:37 +0800

    已完成注册验证码发送的功能 TODO:跳转界面的实现，剩余按钮的完成
    Signed-off-by: JacksonYANG <353904675@qq.com>

--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/ChangeCodeActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/ChangeCodeActivity.java
@@ -2,15 +2,18 @@ package top.jacksonyang.doctorialhat.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import top.jacksonyang.doctorialhat.R;
 
 
 public class ChangeCodeActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_code);
+
     }
 }
--- app/src/main/java/top/jacksonyang/doctorialhat/Activity/LoginActivity.java
+++ app/src/main/java/top/jacksonyang/doctorialhat/Activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package top.jacksonyang.doctorialhat.Activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
